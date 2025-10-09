73616a5414cda5cbcb4f96c3de17759e001570de
Anathyon <anathyon@hotmail.com>
2025-10-09 20:57:33 -0300

    Primeiras configurações do ambiente de trabalho

---
+++ Conversor-de-moedas/src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='flex justify-center bg-amber-950 text-amber-50'>Pêota</h1>
+    </>
+  )
+}
+
+export default App
